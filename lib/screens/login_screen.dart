83ddd53fbfb40e829f757978c7f2538dded49131
Sadid <sadid.jones@gmail.com>
2022-11-11 22:17:31 +0600

    first commit

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,32 @@
+import 'package:docs_clone_flutter/colors.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton.icon(
+          onPressed: () {},
+          icon: Image.asset(
+            'assets/images/g-logo-2.png',
+            height: 20,
+          ),
+          label: const Text(
+            'Sign in with Google',
+            style: TextStyle(color: kBlackColor),
+          ),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kWhiteColor,
+            minimumSize: const Size(
+              150,
+              50,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
