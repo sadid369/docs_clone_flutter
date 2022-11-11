fb2fb2bd5ef35b7a6f1d7a108cfdc495bde16fdb
Sadid <sadid.jones@gmail.com>
2022-11-11 22:18:15 +0600

    @11/11/2022 @27:30

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Docs Clone Flutter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
